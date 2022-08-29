332e8779a57957e3eb16fcc93128abfdca39b395
sechangchoi <scchoi5@naver.com>
2022-08-29 10:03:19 +0900

    Final answer by search google

--- code test/기타줄.py
+++ code test/기타줄.py
@@ -3,16 +3,12 @@ six = []
 one = []
 for i in range(M):
     package, single = map(int, input().split())
-    if package < single * 6:
-        six.append(package)
-    else:
-        six.append(single * 6)
-    if single * (N % 6) < package:
-        one.append(single * (N % 6))
-    else:
-        one.append(package)
-if N <= 6:
-    total = six + one
-    print(min(total))
+    six.append(package)    ## 묶음과 낱개를 각각 append
+    one.append(single)
+if min(six) <= (min(one) * 6):
+    answer = min(six) * (N//6) + min(one) * (N % 6)
+    if min(six) < (min(one) * (N % 6)):
+        answer = min(six) * (N//6 + 1)
 else:
-    print(min(one) + (min(six) * (N // 6)))
+    answer = min(one) * N
+print(answer)
